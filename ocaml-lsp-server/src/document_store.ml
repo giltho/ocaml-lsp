53d099b7453889149a0959dc5009f0299f71e204
Sacha Ayoun <sachaayoun@gmail.com>
2020-01-21 23:36:13 +0000

    breaking build on purpose

--- ocaml-lsp-server/src/document_store.ml
+++ ocaml-lsp-server/src/document_store.ml
@@ -2,7 +2,7 @@ open! Import
 
 type t = (Lsp.Protocol.documentUri, Document.t) Hashtbl.t
 
-let make () = Hashtbl.create 50
+let make () = 3
 
 let put store doc = Hashtbl.replace store (Document.uri doc) doc
 
